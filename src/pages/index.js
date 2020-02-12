be29b11bb3612b55ae0eb15d08220a969071e6dd
John Speck <speckworks@gmail.com>
2020-02-12 17:08:45 -0500

    basic routing gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+        <h1>Whoa!</h1>
+        <span style={{font:"garamonde", color:"tomato", background:"black"}}>
+            hey mama jama! Let's do Dis!
+        </span>
+        <Link to="/page-2">Page 2</Link>
+Hello world!</div>
