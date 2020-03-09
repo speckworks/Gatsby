bf185431f8d691d8b5ee2cd9b98cd0e7105632bb
John Speck <speckworks@gmail.com>
2020-03-09 14:59:51 -0400

    adding a map

---
+++ src/pages/counter.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+class Counter extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {  count: 0}
+    }
+
+    render(){
+        return <div>
+        <h1>Counter</h1>
+        <p>Current Count: {this.state.count}</p>
+        <button onClick={()=>
+        this.setState(
+            { count:this.state.count + 1}
+            )
+            }>plus</button>
+        <button onClick={()=>
+        this.setState(
+            { count:this.state.count - 1}
+            )
+            }>minus</button>
+            <Link to="/">Go Home</Link>
+        </div>
+    }
+}
+
+export default Counter
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,11 @@ export default () => <div>
         <span style={{font:"garamonde", color:"tomato", background:"black"}}>
             hey mama jama! Let's do Dis!
         </span>
+        <br/>
+        <br />
         <Link to="/page-2">Page 2</Link>
+        <br />
+        <Link to="/counter/">counter page</Link>
+        <Link to="/map/">Map Page</Link>
+        
 Hello world!</div>
