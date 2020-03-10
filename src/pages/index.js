03a3ef425f9a0bef9c25929b9e074985320a75bd
John Speck <speckworks@gmail.com>
2020-03-10 11:41:43 -0400

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import React from "react"
 import Link from "gatsby-link"
 
 export default () => <div>
-        <h1>Whoa!</h1>
-        <span style={{font:"garamonde", color:"tomato", background:"black"}}>
-            hey mama jama! Let's do Dis!
+        <h1>Beaches are Us!</h1>
+        <span style={{font:"garamonde", color:"blues", background:"black"}}>
+in the beginning.. there was water.
         </span>
         <br/>
-        <br />
+        <br/>
         <Link to="/page-2">Page 2</Link>
         <br />
         <Link to="/counter/">counter page</Link>
