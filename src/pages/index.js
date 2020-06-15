ad219c1ebadb6b54a56fb8580d8669fc0b18c166
John Speck <speckworks@gmail.com>
2020-06-15 16:32:03 -0400

    images and font added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,48 @@
 import React from "react"
 import Link from "gatsby-link"
+// import image from "gatsby-image"
+import image from "./assets/womanDog.jpeg"
+// import styled from "styled-components"
+// import img from "./assets/kkkart.jpeg"
+
+
+// const StyledImg = styled(img)`
+//         display: block; 
+// `
+// const Wrapper = styled.figure`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `
+
+// const WrappedImg = props => (<Wrapper><StyledImg {...props} /></Wrapper>)
+
+// export default StyledImg
 
 export default () => <div>
-        <h1>Beaches are Us!</h1>
-        <span style={{font:"garamonde", color:"blue", background:"black"}}>
-in the beginning.. there was water.
-        </span>
+        <h1 style={{display: 'flex',  
+                justifyContent:'center', 
+                alignItems:'center',
+                fontFamily:'Archivo+Black',
+                fontSize:"48px"
+                }}>
+                JANICE SPECK
+                </h1>
+                <div style={{
+                display: 'flex',  
+                justifyContent:'center', 
+                alignItems:'center' 
+        }}>
+        <img 
+        src={image} 
+        style={{ height: "50%",
+                width: "50%"
+        }}
+        />
+        </div>
         <br/>
         <br/>
         <Link to="/page-2">Page 2</Link>
         <br />
-        <Link to="/counter/">counter page</Link>
-
-        
-Hello world!</div>
+        <Link to="/counter/">counter page</Link> 
+!</div>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,5 +4,4 @@ import Link from "gatsby-link"
 export default () => <div>
         <h1>Page 2</h1>
         <Link to="/">Go Home</Link>
-
 </div>
